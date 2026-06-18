64e387a4814a6eb29aad68d27c766cc626763c52
nry422 <henrymuller@gmail.com>
2026-06-18 13:11:06 -0300

    criação favoritos e arquivo de notas
    paginas favorecido.php
    e arquivo txt com notas temporarias para bugs, lembranças e etc

---
+++ favorecido_crud.php
@@ -0,0 +1,161 @@
+<?php
+require_once("valida_acesso.php");
+?>
+<?php
+require_once("conexao.php");
+require_once("favorecido_filtro.php");
+
+//operações via ajax
+if (filter_input(INPUT_SERVER, "REQUEST_METHOD") === "POST") {
+    if (!isset($_POST["acao"])) {
+        return;
+    }
+
+    switch ($_POST["acao"]) {
+        case "adicionar":
+            try {
+                $errosAux = "";
+
+                $registro = new stdClass();
+                $registro = json_decode($_POST['registro']);
+                validaDados($registro);
+
+                $sql = "insert into favorecido(nome, usuario_id) VALUES (?, ?) ";
+                $conexao = new PDO("mysql:host=" . SERVIDOR . ";dbname=" . BANCO, USUARIO, SENHA);
+                $pre = $conexao->prepare($sql);
+                $pre->execute(array(
+                    $registro->nome_favorecido,
+                    $registro->usuario_id_favorecido
+                ));
+                print json_encode($conexao->lastInsertId());
+            } catch (Exception $e) {
+                if (isset($_SESSION["erros"])) {
+                    foreach ($_SESSION["erros"] as $chave => $valor) {
+                        $errosAux .= $valor . "<br>";
+                    }
+                }
+                $errosAux .= $e->getMessage();
+                unset($_SESSION["erros"]);
+                echo "Erro: " . $errosAux . "<br>";
+            } finally {
+                $conexao = null;
+            }
+            break;
+        case "editar":
+            try {
+                $errosAux = "";
+
+                $registro = new stdClass();
+                $registro = json_decode($_POST['registro']);
+                validaDados($registro);
+
+                $sql = "update favorecido set nome = ? where id = ? ";
+                $conexao = new PDO("mysql:host=" . SERVIDOR . ";dbname=" . BANCO, USUARIO, SENHA);
+                $pre = $conexao->prepare($sql);
+                $pre->execute(array(
+                    $registro->nome_favorecido,
+                    $registro->id_favorecido
+                ));
+                print json_encode(1);
+            } catch (Exception $e) {
+                foreach ($_SESSION["erros"] as $chave => $valor) {
+                    $errosAux .= $valor . "<br>";
+                }
+                $errosAux .= $e->getMessage();
+                unset($_SESSION["erros"]);
+                echo "Erro: " . $errosAux . "<br>";
+            } finally {
+                $conexao = null;
+            }
+            break;
+        case "excluir":
+            try {
+                $registro = new stdClass();
+                $registro = json_decode($_POST["registro"]);
+
+                $sql = "delete from favorecido where id = ? ";
+                $conexao = new PDO("mysql:host=" . SERVIDOR . ";dbname=" . BANCO, USUARIO, SENHA);
+                $pre = $conexao->prepare($sql);
+                $pre->execute(array(
+                    $registro->id
+                ));
+
+                print json_encode(1);
+            } catch (Exception $e) {
+                echo "Erro: " . $e->getMessage() . "<br>";
+            } finally {
+                $conexao = null;
+            }
+            break;
+        case 'buscar':
+            try {
+                $registro = new stdClass();
+                $registro = json_decode($_POST["registro"]);
+
+                $sql = "select * from favorecido where id = ?";
+                $conexao = new PDO("mysql:host=" . SERVIDOR . ";dbname=" . BANCO, USUARIO, SENHA);
+                $pre = $conexao->prepare($sql);
+                $pre->execute(array(
+                    $registro->id
+                ));
+
+                print json_encode($pre->fetchAll(PDO::FETCH_ASSOC));
+            } catch (Exception $e) {
+                echo "Erro: " . $e->getMessage() . "<br>";
+            } finally {
+                $conexao = null;
+            }
+            break;
+        default:
+            print json_encode(0);
+            return;
+    }
+}
+
+
+//consulta sem ajax
+function listarFavorecido()
+{
+    try {
+        $usuario_id = isset($_SESSION["usuario_id"]) ? $_SESSION["usuario_id"] : 0;
+
+        $sql = "select * from favorecido where usuario_id = ? order by nome";
+        $conexao = new PDO("mysql:host=" . SERVIDOR . ";dbname=" . BANCO, USUARIO, SENHA);
+        $pre = $conexao->prepare($sql);
+        $pre->execute(array(
+            $usuario_id
+        ));
+        $pre->execute();
+
+        return $pre->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        echo "Erro: " . $e->getMessage() . "<br>";
+    } finally {
+        $conexao = null;
+    }
+}
+
+//consulta sem ajax
+function buscarFavorecido(int $id)
+{
+    try {
+        $usuario_id = isset($_SESSION["usuario_id"]) ? $_SESSION["usuario_id"] : 0;
+
+        $sql = "select * from favorecido where id = ? and usuario_id = ?";
+        $conexao = new PDO("mysql:host=" . SERVIDOR . ";dbname=" . BANCO, USUARIO, SENHA);
+        $pre = $conexao->prepare($sql);
+        $pre->execute(array(
+            $id,
+            $usuario_id
+        ));
+        
+
+        return $pre->fetchAll(PDO::FETCH_ASSOC);
+    } catch (Exception $e) {
+        echo "Erro: " . $e->getMessage() . "<br>";
+    } finally {
+        $conexao = null;
+    }
+}
+
+
---
+++ favorecido_filtro.php
@@ -0,0 +1,15 @@
+<?php
+
+function validaDados($registro)
+{
+    $erros = [];
+
+    if (!filter_var($registro->nome_favorecido, FILTER_SANITIZE_STRING)) {
+        $erros["nome_favorecido"] =  "Nome do Favorecido: Campo vazio e ou informação inválida!";
+    }
+
+    if (count($erros) > 0) {
+        $_SESSION["erros"] = $erros;
+        throw new Exception("Erro nas informações!");
+    }
+}
